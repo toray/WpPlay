540ba9d7b21ab4366fe70748c45553b1a6b8fcf8
tbwisk <tbwisk@youyoudeMacBook-Pro-4.local>
2015-09-28 16:20:52 +0800

    add

--- src/com/waps/wanpay/PayTools.java
+++ src/com/waps/wanpay/PayTools.java
@@ -6,48 +6,22 @@ import com.wanpu.pay.PayResultListener;
 import android.content.Context;
 
 public class PayTools {
-	// 应用或游戏商自定义的支付订单(每条支付订单数据不可相同)
-	static String orderId = "";
-	// 用户标识
-	static String userId = "";// userid
-	// 支付商品名称
-	static String goodsName = "测试商品";
-	// 支付金额
-	static float price = 0.0f;
-	// 支付描述
-	static String goodsDesc = "";
-	// 应用或游戏商服务器端回调接口（无服务器可不填写）
-	static String notifyUrl = "";
 	static Context mContext;
 	static String APP_ID;
 	static String APP_PID;
 
-	public static void init(String appid, String app_pid) {
+	public static void init(String appid, String app_pid,Context context) {
 		APP_ID = appid;
 		APP_PID = app_pid;
+		PayConnect.getInstance(APP_ID, APP_PID, context);
 	}
 
-	public static void Pay(float number, Context context, final PayCallBack palycallback) {
+	public static void Pay(final PayBean bean, Context context, final PayCallBack palycallback) {
 		// 初始化统计器(必须调用)
 		PayConnect.getInstance(APP_ID, APP_PID, context);
-
 		mContext = context;
-		goodsDesc = "购买" + goodsName;
-
-		userId = PayConnect.getInstance(context).getDeviceId(context);
-
 		try {
-			// // 游戏商自定义支付订单号，保证该订单号的唯一性，建议在执行支付操作时才进行该订单号的生成
-			orderId = System.currentTimeMillis() + "";
-
-			String amountStr = String.valueOf(number);
-			if (!"".equals(amountStr)) {
-				price = Float.valueOf(amountStr);
-			} else {
-				price = 0.0f;
-			}
-
-			PayConnect.getInstance(context).pay(context, orderId, userId, price, goodsName, goodsDesc, notifyUrl,
+			PayConnect.getInstance(context).pay(context, bean.getOrderId(), bean.getUserid(), bean.getPrice(), bean.getGoodsName(), bean.getGoodsDesc(), bean.getNotifyUrl(),
 					new PayResultListener() {
 
 						@Override
@@ -66,6 +40,9 @@ public class PayTools {
 		} catch (NumberFormatException e) {
 			e.printStackTrace();
 		}
+	}
+	public static void Destory(Context context){
+		PayConnect.getInstance(context).close();
 	}
 
 	public interface PayCallBack {
